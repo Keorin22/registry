cb13a241a441d66d717577e20b2400e01c5d1369
keorin22 <korotenkoiy@gmail.com>
2023-02-19 22:57:26 +0700

    form

---
+++ src/myForm.tsx
@@ -0,0 +1,80 @@
+import React, { useState } from 'react';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+import { format } from 'date-fns';
+
+interface FormValues {
+  selection: string;
+  date: Date | null;
+}
+
+const options = ['Option 1', 'Option 2', 'Option 3'];
+
+export const MyForm: React.FC = () => {
+  const [formValues, setFormValues] = useState<FormValues>({
+    selection: '',
+    date: null,
+  });
+
+  const [list, setList] = useState<string[]>([]);
+
+  const handleSelectionChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setFormValues({
+      ...formValues,
+      selection: event.target.value,
+    });
+  };
+
+  const handleDateChange = (date: Date | null) => {
+    setFormValues({
+      ...formValues,
+      date,
+    });
+  };
+
+  const handleAddToList = () => {
+    const formattedDate = formValues.date ? format(formValues.date, 'dd.MM.yyyy HH:mm') : '';
+    setList([...list, `${formValues.selection} - ${formattedDate}`]);
+    setFormValues({ selection: '', date: null });
+  };
+
+  return (
+    <form>
+      <div style={{ display: 'inline-block' }}>
+        <label>
+          Selection:
+          <select value={formValues.selection} onChange={handleSelectionChange}>
+            <option value="">Select an option</option>
+            {options.map((option) => (
+              <option key={option} value={option}>
+                {option}
+              </option>
+            ))}
+          </select>
+        </label>
+      </div>
+      <div style={{ display: 'inline-block', marginLeft: '10px' }}>
+        <label>
+          Date and Time:
+          <DatePicker
+            selected={formValues.date}
+            onChange={handleDateChange}
+            showTimeSelect
+            timeFormat="HH:mm"
+            timeIntervals={15}
+            dateFormat="MMMM d, yyyy hh:mm"
+          />
+        </label>
+      </div>
+      <button type="button" onClick={handleAddToList}>
+        Add to list
+      </button>
+      <ul>
+        {list.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
+    </form>
+  );
+};
+
